36003f3051f0c484a33615ae4a24012c509d7ec4
Michele Costa <152905633+michele2c@users.noreply.github.com>
2023-12-14 15:56:48 -0700

    Update ec2-random-name-gen.py

--- ec2-random-name-gen.py
+++ ec2-random-name-gen.py
@@ -18,32 +18,29 @@ Project - EC2 Random Name Generator
 
 # 4. Push your code to GitHub and include the link in your LinkedIn write up.
 
-# marketing-e7b1682894434ec5890312c7c403c011
+# example of format from AWS -> name-e7b1682894434ec5890312c7c403c011
 
 import random
 import string
 
-department_name = str(input("Department: "))
-number_instances = int(input("Number of instances: "))
 
-# initialize an empty list
-instances_list = []
-# initialize while loop counter
-counter = 1
+num_instances = int(input("Number of instances: "))
+dep_name = str(input("Department name: "))
 
-while counter <= number_instances:
-    # initializing size of string
-    string_size = 32
-    # use random.choices() to randomly choose characters and numbers
-    # ''.join() to join the list of characters
-    random_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=string_size))
-    # store department name in lowercase + random string
-    instances = department_name.lower() + "-" + random_name
-    # append result to the list
-    instances_list.append(instances)
- 
-    counter += 1 #increment by 1
-
-
-print(instances_list)
-
+# validate department name
+if dep_name.lower() == "marketing" or dep_name.lower() == "accounting" or dep_name.lower() == "finops":
+  # initialize list
+    instances_list = []
+    for x in range(num_instances):
+      x += 1
+      # define the lenght of the string
+      str_size = 15
+      # random part of the instance name
+      random_name = ''.join(random.choices(string.hexdigits, k=str_size))
+      # combine department and random name
+      instance_name = dep_name.lower() + "-" + random_name.lower()
+      # append names to the list
+      instances_list.append(instance_name)
+    print(instances_list)
+else:
+    print("Sorry! Your department cannot use this tool.")
