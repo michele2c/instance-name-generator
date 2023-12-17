2efe7bc72650ced1302ab64599bf03271c83fef7
Michele Costa Christianson <michpimentel22@gmail.com>
2023-12-17 14:04:08 -0700

    test file

---
+++ tests.py
@@ -0,0 +1,9 @@
+
+
+# # use this example tp make a list for departments for the project
+# dep_names = ['marketing', 'accounting', 'finops']
+# dep_name = str.lower(input('Department: '))
+# if dep_name not in dep_names:
+#   print('Ooops... Your department cannot use this tool.')
+# # else:
+#call function
